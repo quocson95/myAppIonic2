06d6df3880bf467ce3775d1053410daf4cd91a24
= <=>
2017-04-05 14:16:12 +0700

    creat history menu

---
+++ src/pages/Menu/History/history.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-menu-history',
+  templateUrl: 'history.html'
+})
+
+
+export class Menu {
+
+  constructor(public navCtrl: NavController) {
+  }
+}
